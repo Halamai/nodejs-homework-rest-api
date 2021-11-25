58e97a11d6c14fe4004e4d8b0e6263e8f710245e
Halamai <halamaipawlo@gmail.com>
2021-11-25 21:40:14 +0200

    2222

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -93,4 +93,30 @@ router.put("/:contactId", validation, async (req, res, next) => {
   }
 });
 
+// @ PATCH /api/contacts/:contactId/favorite
+
+router.patch("/:contactId/favorite", validation, async (req, res, next) => {
+  const { contactId } = req.params;
+  try {
+    if (!Object.keys(req.body).includes("favorite")) {
+      const error = new Error("missing field favorite");
+      error.status = 400;
+      throw error;
+    }
+    const data = await functions.updateContactStatus(contactId, req.body);
+    if (!data) {
+      const error = new Error("Not found");
+      error.status = 404;
+      throw error;
+    }
+    res.status(200).json({
+      status: "success",
+      code: 201,
+      data: data,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
